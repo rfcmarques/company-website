1b8b895c4660f6cbcf404284deec22468e7553b4
Rui Marques <a.marques1967@gmail.com>
2024-02-18 02:05:30 +0000

    Removed forgotten console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ function userScroll() {
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
-            console.log('aqui')
             navbar.classList.add('navbar-sticky');
         } else {
             navbar.classList.remove('navbar-sticky');
