0934e470afcb1390011e9c0b4cddf7daa2d5730d
Rui Marques <a.marques1967@gmail.com>
2024-02-18 02:40:49 +0000

    Created the function for top top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
 function userScroll() {
     const navbar = document.querySelector('.navbar');
+    const toTopBtn = document.querySelector('#to-top');
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
             navbar.classList.add('navbar-sticky');
+            toTopBtn.classList.add('show');
         } else {
             navbar.classList.remove('navbar-sticky');
+            toTopBtn.classList.remove('show');
         }
     })
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 function incrementStats() {
@@ -36,3 +44,4 @@ function incrementStats() {
 
 document.addEventListener('DOMContentLoaded', userScroll);
 document.addEventListener('DOMContentLoaded', incrementStats);
+document.querySelector('#to-top').addEventListener('click', scrollToTop);
